604860379666682e1f5eeca068dbb1fa0dfb5cfe
lukkearmageddon <lukkemail@gmail.com>
2023-04-16 19:53:20 +0200

    feat: create new pages for app.module.

--- src/app/routing/website/routing/recipetoria-app/routing/app/app.module.ts
+++ src/app/routing/website/routing/recipetoria-app/routing/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppPageComponent } from './pages/app-page/app-page.component';
 import { RouterModule } from '@angular/router';
+import { ShopListPageComponent } from './pages/shop-list-page/shop-list-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 
 @NgModule({
-	declarations: [AppPageComponent],
+	declarations: [AppPageComponent, ShopListPageComponent, HeaderComponent, FooterComponent, ShoppingListComponent],
 	imports: [
 		CommonModule,
 		RouterModule.forChild([
---
+++ src/app/routing/website/routing/recipetoria-app/routing/app/pages/shop-list-page/shop-list-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-list-page',
+  templateUrl: './shop-list-page.component.html',
+  styleUrls: ['./shop-list-page.component.scss']
+})
+export class ShopListPageComponent {
+
+}
