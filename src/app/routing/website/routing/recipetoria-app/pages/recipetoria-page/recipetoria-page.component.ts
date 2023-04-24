cd873149ad72b5335e7376f71160ffb38499b93b
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-04-24 20:51:40 +0300

    refactor: clean all files

--- src/app/routing/website/routing/recipetoria-app/pages/recipetoria-page/recipetoria-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-recipetoria-page',
-	templateUrl: './recipetoria-page.component.html',
-	styleUrls: ['./recipetoria-page.component.scss']
-})
-export class RecipetoriaPageComponent {}
