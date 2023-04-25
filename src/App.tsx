619e6248bee234d87e7249030b977e186a8e865c
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-04-25 17:26:22 +0300

    feat: add shopping list page, header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.scss";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import ShoppingListPage from "./pages/ShoppingListPage/ShoppingListPage";
 
 function App() {
-  return <div className="app" />;
+  return (
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path="shopping_list" element={<ShoppingListPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/ShoppingListPage/ShoppingListPage.tsx
@@ -0,0 +1,5 @@
+import "./ShoppingListPage.scss";
+
+export default function ShoppingListPage() {
+  return <>Shopping list page</>;
+}
