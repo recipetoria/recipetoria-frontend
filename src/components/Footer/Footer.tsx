0339cfbebdf4748149ae96e1830bc2e55cc5a78b
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-04-25 20:04:27 +0300

    feat: add style for footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,22 @@
+import Logo from "../../assets/svg/Logo";
 import "./Footer.scss";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__wrapper">
+        <div className="logo__wrapper">
+          <Logo />
+        </div>
+        <article className="footer-links-n-rights">
+          <a href="/#">Privacy policy</a>
+          <a href="/#">Cookie policy</a>
+          <a href="/#">Terms of use</a>
+          <span className="footer-links-n-rights__right">
+            All right reserved
+          </span>
+        </article>
+      </div>
+    </footer>
+  );
 }
