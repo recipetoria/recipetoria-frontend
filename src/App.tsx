4dcb1c55490c11dfe0a32a530ee8d122dddd56d9
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-02 16:57:52 +0300

    feat: start layout and refactor sign page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import ShoppingListPage from "./pages/ShoppingListPage/ShoppingListPage";
 import AllCategoriesPage from "./pages/AllCategoriesPage/AllCategoriesPage";
 import AddRecipePage from "./pages/AddRecipePage/AddRecipePage";
-import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import SignPage from "./pages/SignUpPage/SignUpPage";
 
 function App() {
   return (
     <div className="app">
       <Routes>
-        <Route path="sign_up" element={<SignUpPage />} />
+        <Route path="sign_up" element={<SignPage signMode="signUp" />} />
+        <Route path="sign_in" element={<SignPage signMode="signIn" />} />
         <Route path="all_categories" element={<AllCategoriesPage />} />
         <Route path="shopping_list" element={<ShoppingListPage />} />
         <Route path="add_recipe" element={<AddRecipePage />} />
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,3 +1,28 @@
-export default function SignUpPage() {
-  return <>Sign Up Page</>;
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
+type SignMode = "signUp" | "signIn";
+
+interface ISignPageProps {
+  signMode: SignMode;
+}
+
+export default function SignPage(props: ISignPageProps) {
+  const { signMode } = props;
+
+  return (
+    <main className="main">
+      <article className="sign-page">
+        <section className="sign-page__img-wrapper">
+          <img src="" alt="" />
+        </section>
+        <section className="sign-page__form-n-header">
+          <h2>Welcome to Reciptopedia</h2>
+          <form>
+            <label htmlFor="nickname">Nickname</label>
+            <input type="text" id="nickname" />
+          </form>
+        </section>
+      </article>
+    </main>
+  );
 }
