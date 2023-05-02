6520ebf7eeb960e3deabf0947ef483dd66822688
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-02 19:07:43 +0300

    fix: add request for server

--- src/pages/AllCategoriesPage/AllCategoriesPage.tsx
+++ src/pages/AllCategoriesPage/AllCategoriesPage.tsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
 import "./AllCategoriesPage.scss";
 
 export default function AllCategoriesPage() {
-  return <main>All Categories</main>;
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      const data = await (
+        await fetch(
+          "https://recipetoria-production.up.railway.app/api/v1/auth/verify-email?token=eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJyZWNpcGV0b3JpYSIsInN1YiI6InB1a2Z1a0BtYWlsLmNvbSIsImlhdCI6MTY4Mjg1OTY4NywiZXhwIjoxNjgyOTQ2MDg3fQ.XFGD_QsQcNn3RudQ50lZ2rUxE9WCklkpCQbFRiBmFr8"
+        )
+      ).json();
+      setState(data);
+    };
+
+    dataFetch();
+  }, []);
+
+  return (
+    <main>
+      <h1>All Categories</h1>
+      <span>Checking the request for success: {state}</span>
+    </main>
+  );
 }
