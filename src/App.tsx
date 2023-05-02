a9cfecce26a01abbb32a09268081a87430e0c12b
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-02 15:54:44 +0300

    feat: add sign up page and route

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import ShoppingListPage from "./pages/ShoppingListPage/ShoppingListPage";
 import AllCategoriesPage from "./pages/AllCategoriesPage/AllCategoriesPage";
 import AddRecipePage from "./pages/AddRecipePage/AddRecipePage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
 
 function App() {
   return (
     <div className="app">
       <Routes>
+        <Route path="sign_up" element={<SignUpPage />} />
         <Route path="all_categories" element={<AllCategoriesPage />} />
         <Route path="shopping_list" element={<ShoppingListPage />} />
         <Route path="add_recipe" element={<AddRecipePage />} />
