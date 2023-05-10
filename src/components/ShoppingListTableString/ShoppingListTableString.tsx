41dd3639ebb877e7274e6a19cb43eb6d41943b79
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-10 23:10:09 +0300

    feat: style add tip popup

--- src/components/ShoppingListTableString/ShoppingListTableString.tsx
+++ src/components/ShoppingListTableString/ShoppingListTableString.tsx
@@ -144,6 +144,11 @@ export default function ShoppingListTableString(
         >
           <Trash />
         </button>
+        {hoverTrashId && (
+          <div className="popup-tip">
+            <span>Delete</span>
+          </div>
+        )}
       </div>
     </>
   );
