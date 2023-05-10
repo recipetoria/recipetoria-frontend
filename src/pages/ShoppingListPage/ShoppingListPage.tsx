bf150b89d8880204bb3473066e2fdbb7e6e927c4
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-10 21:11:54 +0300

    feat: add request for test

--- src/pages/ShoppingListPage/ShoppingListPage.tsx
+++ src/pages/ShoppingListPage/ShoppingListPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Print from "../../assets/svg/Print";
 import Share from "../../assets/svg/Share";
 import Trash from "../../assets/svg/Trash";
@@ -7,7 +8,37 @@ import Header from "../../components/Header/Header";
 import ShoppingListTable from "../../components/ShoppingListTable/ShoppingListTable";
 import "./ShoppingListPage.scss";
 
+interface IResponse {
+  developerMessage: string;
+  localDateTime: string;
+  message: string;
+  path: string;
+  statusCode: number;
+}
+
 export default function ShoppingListPage() {
+  const [stateShop, setStateShop] = useState<IResponse>();
+
+  useEffect(() => {
+    const dataFetchShop = async () => {
+      const dataShopGet = await (
+        await fetch(
+          "https://recipetoria-production.up.railway.app/api/v1/client",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJyZWNpcGV0b3JpYSIsInN1YiI6IkhlbGVuX01jR2x5bm42NkBnbWFpbC5jb20iLCJpYXQiOjE2ODM0ODMzMjgsImV4cCI6MTY4MzU2OTcyOH0.JzSXC2KKo4uCE3nIiJSgut1KSScqibjV2dHYgxFfxGU`,
+            },
+          }
+        )
+      ).json();
+      setStateShop(dataShopGet);
+    };
+    setTimeout(() => {
+      dataFetchShop();
+    }, 1000);
+  }, []);
+  console.log(stateShop);
   return (
     <>
       <Header />
