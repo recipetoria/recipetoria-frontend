2b48f19013a0504db74a8efacfc4471413c8b83d
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-11 13:34:31 +0300

    feat: add btn undo and cross

--- src/components/Snackbar/Snackbar.tsx
+++ src/components/Snackbar/Snackbar.tsx
@@ -6,6 +6,10 @@ export default function Snackbar(props: { text: string }) {
   return (
     <aside className="snackbar">
       <span className="snackbar__text">{text}</span>
+      <section className="snackbar__btns">
+        <button type="button">Undo</button>
+        <button type="button">+</button>
+      </section>
     </aside>
   );
 }
