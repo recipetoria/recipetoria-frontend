2cc1393b85227158787f4982a5633de0f1a696fa
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-11 13:54:57 +0300

    feat: style for btn in snackbar

--- src/components/Snackbar/Snackbar.tsx
+++ src/components/Snackbar/Snackbar.tsx
@@ -1,3 +1,4 @@
+import CrossIcon from "../../assets/svg/CrossIcon";
 import "./Snackbar.scss";
 
 export default function Snackbar(props: { text: string }) {
@@ -7,8 +8,12 @@ export default function Snackbar(props: { text: string }) {
     <aside className="snackbar">
       <span className="snackbar__text">{text}</span>
       <section className="snackbar__btns">
-        <button type="button">Undo</button>
-        <button type="button">+</button>
+        <button type="button" className="snackbar__btn snackbar__btn_undo">
+          Undo
+        </button>
+        <button type="button" className="snackbar__btn snackbar__btn_cross">
+          <CrossIcon />
+        </button>
       </section>
     </aside>
   );
