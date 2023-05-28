8689ae5fdecde7fd71f2b49a776d3114246f99a2
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-29 00:03:01 +0300

    refactor: change isAuth to slice in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,10 +2,11 @@ import { Link } from "react-router-dom";
 import "./Header.scss";
 import Logo from "../../assets/svg/Logo";
 import DefaultAvatar from "../../assets/png/default_ava.png";
+import { useAppSelector } from "../../app/hooks";
 
 export default function Header() {
-  const STORAGE_AUTH = localStorage.getItem("authRegister");
-  const IS_AUTH = STORAGE_AUTH ? JSON.parse(STORAGE_AUTH).isAuth : "";
+  const isAuth = useAppSelector((state) => state.present.authData.value.isAuth);
+
   return (
     <header className="header">
       <div className="header__wrapper">
@@ -16,7 +17,7 @@ export default function Header() {
         </Link>
         <article className="links-n-avatar">
           <section className="links">
-            {IS_AUTH ? (
+            {isAuth ? (
               <>
                 <Link to="/all_categories">All Categories</Link>
                 <Link to="/shopping_list">Shopping list</Link>
@@ -35,7 +36,7 @@ export default function Header() {
               </>
             )}
           </section>
-          {IS_AUTH && (
+          {isAuth && (
             <div className="default-avatar__wrapper">
               <img src={DefaultAvatar} alt="" className="default-avatar" />
             </div>
