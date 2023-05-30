143a7dc769959f76add5bc0a9e6ae0c8504dfd68
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-30 14:11:26 +0300

    feat: add basic layout for add profile photo

--- src/components/AddProfilePhoto/AddProfilePhoto.tsx
+++ src/components/AddProfilePhoto/AddProfilePhoto.tsx
@@ -1,5 +1,19 @@
 import "./AddProfilePhoto.scss";
 
 export default function AddProfilePhoto() {
-  return <div>Puk</div>;
+  return (
+    <section className="add-profile-photo">
+      <h3 className="add-profile-photo__header">Add profile photo</h3>
+      <section className="add-photo">Add photo</section>
+      <span>or</span>
+      <section className="add-profile-photo__btns">
+        <button type="button" className="add-profile-photo__cancel">
+          Cancel
+        </button>
+        <button type="button" className="add-profile-photo__upload">
+          Upload picture
+        </button>
+      </section>
+    </section>
+  );
 }
