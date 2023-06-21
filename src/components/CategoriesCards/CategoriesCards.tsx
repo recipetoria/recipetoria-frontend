3f73a69ee6e164cb86481c424b455766eef411d6
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-21 18:23:58 +0300

    feat: categories page

--- src/components/CategoriesCards/CategoriesCards.tsx
+++ src/components/CategoriesCards/CategoriesCards.tsx
@@ -2,13 +2,18 @@ import allTagsFakeDataArr from "../../assets/data/allTagsFakeData";
 import DefaultCategoryPhoto from "../../assets/png/no_photo_categ.png";
 import CreateNewCategoryImage from "../../assets/png/create_new_category.png";
 import PlusIcon from "../../assets/svg/PlusIcon";
+import "./CategoriesCards.scss";
 
 export default function CategoriesCards() {
   const categoriesCardsJsx = allTagsFakeDataArr.map((item) => (
     <section className="card" key={item.name} id={item.id.toString()}>
       <div className="card__wrapper">
         <section className="card__image-wrapper">
-          <img src={item.mainPhoto || DefaultCategoryPhoto} alt="category" />
+          <img
+            src={item.mainPhoto || DefaultCategoryPhoto}
+            alt="category"
+            className="card__image"
+          />
         </section>
         <h4 className="card__name">{item.name}</h4>
       </div>
@@ -20,13 +25,15 @@ export default function CategoriesCards() {
       <section className="card">
         <div className="card__wrapper">
           <section className="card__image-wrapper">
-            <img src={CreateNewCategoryImage} alt="create new category" />
+            <img
+              src={CreateNewCategoryImage}
+              alt="create new category"
+              className="card__image card__image_create"
+            />
           </section>
           <section className="card__create-block">
             <PlusIcon />
-            <span className="card__name card__name_create">
-              Create new category
-            </span>
+            <span className="card__create-text">Create new category</span>
           </section>
         </div>
       </section>
