eca46bb2a8eba63ae97b40f2aec9cb9bd741282d
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-21 16:23:30 +0300

    feat: create fake data

---
+++ src/assets/data/allTagsFakeData.ts
@@ -0,0 +1,37 @@
+import fakePhoto from "./tagsFakePhotos/photo.png";
+
+const names = [
+  "Chicken",
+  "Dinners",
+  "Gluten-free",
+  "Meat",
+  "Mom`s recipes",
+  "Oven",
+  "Passover",
+  "Passover",
+  "Quick recipes",
+  "Romantic recipes",
+  "Spaghetti",
+  "Summer dishes",
+  "Uncategorized",
+];
+
+const allTagsFakeDataArr: {
+  id: number;
+  name: string;
+  mainPhoto: string | null;
+  applicationUserId: number;
+  recipeIds: [];
+}[] = [];
+
+names.map((item, id) =>
+  allTagsFakeDataArr.push({
+    id,
+    name: item,
+    mainPhoto: id % 2 ? fakePhoto : null,
+    applicationUserId: 1,
+    recipeIds: [],
+  })
+);
+
+export default allTagsFakeDataArr;
