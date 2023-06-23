3ec43c898e50e197f558cf192324527dd677dfe4
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-23 14:38:37 +0300

    feat: add hover for cards

---
+++ src/assets/svg/PencilIcon.tsx
@@ -0,0 +1,16 @@
+export default function PencilIcon() {
+  return (
+    <svg
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.06 9.5L15 10.44L5.92 19.5H5V18.58L14.06 9.5ZM17.66 3.5C17.41 3.5 17.15 3.6 16.96 3.79L15.13 5.62L18.88 9.37L20.71 7.54C21.1 7.15 21.1 6.5 20.71 6.13L18.37 3.79C18.17 3.59 17.92 3.5 17.66 3.5ZM14.06 6.69L3 17.75V21.5H6.75L17.81 10.44L14.06 6.69Z"
+        fill="black"
+      />
+    </svg>
+  );
+}
--- src/components/CategoriesCards/CategoriesCards.tsx
+++ src/components/CategoriesCards/CategoriesCards.tsx
@@ -1,10 +1,13 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { Link } from "react-router-dom";
 import allTagsFakeDataArr from "../../assets/data/allTagsFakeData";
 import DefaultCategoryPhoto from "../../assets/png/no_photo_categ.png";
 import CreateNewCategoryImage from "../../assets/png/create_new_category.png";
 import PlusIcon from "../../assets/svg/PlusIcon";
 import "./CategoriesCards.scss";
 import CreateNewCategory from "../CreateNewCategory/CreateNewCategory";
+import PencilIcon from "../../assets/svg/PencilIcon";
+import ArrowIcon from "../../assets/svg/ArrowIcon";
 
 interface CategoriesCardsProps {
   toggle: () => void;
@@ -14,12 +17,29 @@ interface CategoriesCardsProps {
 export default function CategoriesCards(props: CategoriesCardsProps) {
   const { toggle, modalChildren } = props;
 
+  // const [isHoverCategory, SetIsHoverCategory] = useState<{
+  //   id: number;
+  //   isHover: boolean;
+  // }>();
+
   const categoriesCardsJsx = allTagsFakeDataArr.map((item) => (
-    <button
-      type="button"
+    <Link
+      to="/"
       className="card"
       key={item.name}
       id={item.id.toString()}
+      onMouseEnter={() => {
+        // SetIsHoverCategory({
+        //   id: item.id,
+        //   isHover: true,
+        // });
+      }}
+      onMouseLeave={() => {
+        // SetIsHoverCategory({
+        //   id: item.id,
+        //   isHover: false,
+        // });
+      }}
     >
       <div className="card__wrapper">
         <section className="card__image-wrapper">
@@ -30,8 +50,12 @@ export default function CategoriesCards(props: CategoriesCardsProps) {
           />
         </section>
         <h4 className="card__name">{item.name}</h4>
+        <button type="button" className="menu-btn">
+          <PencilIcon />
+          <ArrowIcon />
+        </button>
       </div>
-    </button>
+    </Link>
   ));
 
   return (
