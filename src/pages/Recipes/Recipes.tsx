9a44d0150b912d678ffe04fd03d042a5e45ba736
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-07-04 23:57:06 +0300

    feat: add check if tag exist

--- src/pages/Recipes/Recipes.tsx
+++ src/pages/Recipes/Recipes.tsx
@@ -1,7 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAppSelector } from "../../app/hooks";
 
 export default function Recipes() {
   const { id } = useParams();
+
+  const navigate = useNavigate();
+
+  const tags = useAppSelector((state) => state.present.tags.value);
+
+  const foundIdInTags = id
+    ? tags.map((tag) => tag.name === id).includes(true)
+    : false;
+
+  useEffect(() => {
+    if (!foundIdInTags) {
+      navigate("/*");
+    }
+  });
 
   return (
     <main>
