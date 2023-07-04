b034c4c5df708e804ea03120a795fdbe2cc06304
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-07-04 18:40:07 +0300

    refactor: replace categories into separate component

--- src/pages/AllCategoriesPage/AllCategoriesPage.tsx
+++ src/pages/AllCategoriesPage/AllCategoriesPage.tsx
@@ -1,14 +1,14 @@
-import { ReactNode, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import "./AllCategoriesPage.scss";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import CategoriesCards from "../../components/CategoriesCards/CategoriesCards";
 import Modal from "../../components/Modal/Modal";
 import useModal from "../../hooks/useModal";
 import Snackbar from "../../components/Snackbar/Snackbar";
 import { fetchTags } from "../../features/CategorySlice";
+import { ModalContentContext } from "../../contexts/ModalContentContext";
 
 export default function AllCategoriesPage() {
   const isAuth = useAppSelector(
@@ -16,8 +16,8 @@ export default function AllCategoriesPage() {
   );
   const isOpen = useAppSelector((state) => state.present.IsOpenModal.value);
   const { toggle } = useModal();
-  const [modalChildren, setModalChildren] = useState<ReactNode>(<div />);
   const token = useAppSelector((state) => state.present.authData.value.token);
+  const { modalContent } = useContext(ModalContentContext);
 
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
@@ -37,14 +37,10 @@ export default function AllCategoriesPage() {
           <main>
             <article className="categories-page">
               <div className="categories-page__wrapper">
-                <h2 className="categories-page__h2">Categories</h2>
-                <CategoriesCards
-                  toggle={toggle}
-                  modalChildren={(modalChild) => setModalChildren(modalChild)}
-                />
+                <Outlet />
               </div>
               <Modal isOpen={isOpen} toggle={toggle}>
-                {modalChildren}
+                {modalContent}
               </Modal>
               <Snackbar />
             </article>
