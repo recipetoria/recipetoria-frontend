95927e9f29b86ae288d598f7e02a9e80f57a762f
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-07-06 17:44:39 +0300

    refactor: add comment todo

--- src/components/CategoriesCards/CategoriesCards.tsx
+++ src/components/CategoriesCards/CategoriesCards.tsx
@@ -27,6 +27,7 @@ export default function CategoriesCards(props: CategoriesCardsProps) {
   } else if (tagsError) {
     categoriesCardsJsx = <h3>Something went wrong</h3>;
   } else if (tagsValue.length) {
+    // TODO: add sort by tag name
     categoriesCardsJsx = tagsValue.map((item) => (
       <CategoryCard
         id={item.id}
