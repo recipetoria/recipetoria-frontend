3ec4c8ceabf16b4b502c7a744e72394fffd639b5
Alya <112481781+AlyaKavalenka@users.noreply.github.com>
2023-08-25 14:23:17 +0300

    Adaptive 2 (#28)
    * feat: disappear snackbar after 5sec
    
    * feat: recipe page 1279px
    
    * feat: recipe page 1439px
    
    * feat: style categories/recipes 904px
    
    * feat: style categories/recipes 1279px
    
    * feat: style categories/recipes 1439px
    
    * feat: style start page 600px
    
    * fix: style avatar in profile
    
    * feat: hide undo
    
    * feat: style start page 1279px

--- src/components/Snackbar/Snackbar.tsx
+++ src/components/Snackbar/Snackbar.tsx
@@ -13,13 +13,16 @@ export default function Snackbar() {
     (state) => state.present.snackbarTextSlice.value.withUndo
   );
 
-  // TODO: disappear after some time
+  setTimeout(() => {
+    dispatch(SnackbarTextValue({ text: "", withUndo: false }));
+  }, 10000);
 
   return (
     <aside className={`snackbar snackbar_${text.length > 0 ? "show" : "hide"}`}>
       <span className="snackbar__text">{text}</span>
       <section className="snackbar__btns">
-        {isUndoBtn && (
+        {/* TODO: remove false when undo will be done in server side */}
+        {isUndoBtn && false && (
           <button
             type="button"
             className="snackbar__btn snackbar__btn_undo"
@@ -31,10 +34,12 @@ export default function Snackbar() {
             Undo
           </button>
         )}
+        {/* TODO: remove false and eslint comment when undo will be done in server side */}
         <button
           type="button"
           className={`snackbar__btn ${
-            isUndoBtn
+            // eslint-disable-next-line no-constant-condition
+            isUndoBtn && false
               ? "snackbar__btn_cross"
               : "snackbar__btn_cross-without-undo"
           }`}
